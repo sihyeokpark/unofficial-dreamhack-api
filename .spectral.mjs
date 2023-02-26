99b143f512a593b2622ed32543b6939c4d2bae4e
EXON <exon.indiv@gmail.com>
2023-02-26 20:20:05 +0900

    Modified 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4NzEzNA/spectral.js?branch_slug=master&token=2f3c8275-7ddd-44af-b8f5-6f07e109557e";
+export default { extends: ruleset };
